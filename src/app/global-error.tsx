3957f17106a86818130a1e1809a4f3c219e20fa4
eliott-herbert-byrnes <eliott.c.h.byrnes@googlemail.com>
2025-12-17 19:43:27 +0000

    linting fix

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,6 +3,7 @@
 import NextError from "next/error";
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+  console.error(error);
   return (
     <html>
       <body>
