972fc0ac3af332c3f43b092e4b5f9f86ed3e5308
eliott-herbert-byrnes <eliott.c.h.byrnes@googlemail.com>
2026-02-14 18:37:37 +0000

    chore:minor-version-error

--- src/features/procedures/utils/get-initial-content-for-style.ts
+++ src/features/procedures/utils/get-initial-content-for-style.ts
@@ -18,8 +18,7 @@ export const getInitialContentForStyle = (style: ProcedureStyle) => {
       };
     case "FLOW":
       return {
-        nodes: [],
-        edges: [],
+        nodes: [], edges: [], 
         tiptap: {
           type: "doc",
           content: [
