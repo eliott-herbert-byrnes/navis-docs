bc07ea04fd45690a0840cfd935a9a7c2795570e6
eliott-herbert-byrnes <eliott.c.h.byrnes@googlemail.com>
2026-02-27 11:54:47 +0000

    init: inngest function, procedure-import event, import-procedure payload

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { eventCreateOrganization } from "@/features/onboarding/events/event-create-organization";
+import { eventImportProcedure } from "@/inngest/functions/import-procedure";
 
 export const { GET, PUT, POST } = serve({
   client: inngest,
-  functions: [eventCreateOrganization],
+  functions: [eventCreateOrganization, eventImportProcedure],
 });
--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,8 +1,10 @@
 import type { EventCreateOrganization } from "@/features/onboarding/events/event-create-organization";
 import { EventSchemas, Inngest } from "inngest";
+import { EventImportProcedure } from "./functions/import-procedure";
 
 type Events = {
   "onboarding/create-organization": EventCreateOrganization;
+  "procedure/import-file": EventImportProcedure;
 };
 
 export const inngest = new Inngest({
