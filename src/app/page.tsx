26d046d4f027d733eec580a22c4bff4b6aa06c95
eliott-herbert-byrnes <eliott.c.h.byrnes@googlemail.com>
2026-03-06 15:42:47 +0000

    init:audit tRPC route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Heading } from "@/components/ui/Heading";
 import { getSessionUser, getUserOrgWithRole } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { signInPath } from "./paths";
+import { dashboardPath, signInPath } from "./paths";
 import { DepartmentList } from "@/features/departments/components/department-list";
 import { DepartmentCreateButton } from "@/features/departments/components/department-buttons/department-create-button";
 import { Suspense } from "react";
@@ -13,9 +13,10 @@ export async function generateStaticParams() {
 
 export default async function Home() {
   const user = await getSessionUser();
-
   const { org, isAdmin } = await getUserOrgWithRole(user?.userId ?? "");
+
   if (!org || !user) redirect(signInPath());
+
 
   return (
     <>
